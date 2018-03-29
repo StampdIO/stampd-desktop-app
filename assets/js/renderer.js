5dc2149dd9ce1bb071635833afdadc2b9b5d0f6a
antonios <anton.minas@gmail.com>
2018-03-29 14:38:12 +0300

    add filename meta, proper declarations

--- assets/js/renderer.js
+++ assets/js/renderer.js
@@ -6,21 +6,21 @@ const SHA256 = require("crypto-js/sha256");
 // On ready, init
 $(function () {
 
-  var HASH_PREFIX = '';
+  const HASH_PREFIX = '';
   // var API_URL = 'http://dev.stampd.io/api/v2.php';
-  var API_URL = 'https://stampd.io/api/v2.php';
+  const API_URL = 'https://stampd.io/api/v2.php';
 
-  var $blockchain = $('[name="blockchain"]');
-  var $client_id = $('[name="client_id"]');
-  var $secret_key = $('[name="secret_key"]');
+  const $blockchain = $('[name="blockchain"]');
+  const $client_id = $('[name="client_id"]');
+  const $secret_key = $('[name="secret_key"]');
 
-  var $stamp_form = $(".js-form-stamp");
-  var $set_form = $(".js-form-credentials");
+  const $stamp_form = $(".js-form-stamp");
+  const $set_form = $(".js-form-credentials");
 
-  var $tabs = $('[data-tab-btn]');
-  var $tabContents = $('[data-tab-content]');
+  const $tabs = $('[data-tab-btn]');
+  const $tabContents = $('[data-tab-content]');
 
-  var currentStamp = {};
+  let currentStamp = {};
 
   // Tabs
   // =========================================================
@@ -28,7 +28,7 @@ $(function () {
   $tabs.click(function (e) {
     e.preventDefault();
 
-    var $el = $(this);
+    let $el = $(this);
 
     if ($el.hasClass('is-active')) {
       return;
@@ -45,13 +45,13 @@ $(function () {
   // Show notification
   // =========================================================
 
-  var display_notification = function (text, type) {
+  const display_notification = function (text, type) {
     if (typeof type === 'undefined') {
       type = 'default';
     }
 
-    var $notif = $('.js-notification');
-    var $notif_cont = $('.js-notification__content');
+    let $notif = $('.js-notification');
+    let $notif_cont = $('.js-notification__content');
 
     $notif.attr('data-notification-type', type);
 
@@ -66,14 +66,14 @@ $(function () {
   // Sign in
   // =========================================================
 
-  var sign_in = function (client_id, secret_key, cb) {
+  const sign_in = function (client_id, secret_key, cb) {
 
     $.get(API_URL, {
       requestedURL: "init",
       client_id: client_id,
       secret_key: secret_key,
     }, function (res) {
-      var res_json = JSON.parse(res);
+      let res_json = JSON.parse(res);
       cb(res_json.code ? res_json.code : false);
     }).fail(function (res) {
       cb(false);
@@ -83,19 +83,18 @@ $(function () {
   // Post hash
   // =========================================================
 
-  var post_hash = function (hash, blockchain, cb) {
+  const post_hash = function (hash, blockchain, filename, cb) {
 
     $.post(API_URL, {
       requestedURL: "hash",
       hash: hash,
       blockchain: blockchain,
       meta_category: 'Desktop App',
-      // meta_filename
-      // meta_category
+      meta_filename: filename,
       // meta_notes
       // meta_emails
     }, function (res) {
-      var res_json = JSON.parse(res);
+      let res_json = JSON.parse(res);
       cb(res_json ? res_json : false);
     }).fail(function (res) {
       cb(false);
@@ -105,18 +104,18 @@ $(function () {
   // Calculate hash
   // =========================================================
 
-  var calc_hash = function (data, cb) {
-    cb(SHA256(data));
+  const calc_hash = function (data, cb, filename) {
+    cb(SHA256(data), filename);
   };
 
   // Retrieved hash
   // =========================================================
 
-  var retrieved_hash = function (hash) {
+  const retrieved_hash = function (hash, filename) {
 
     currentStamp.hash = HASH_PREFIX + hash;
 
-    post_hash(currentStamp.hash, currentStamp.blockchain, function (res) {
+    post_hash(currentStamp.hash, currentStamp.blockchain, filename, function (res) {
 
       if (!res) {
         $stamp_form.removeClass('is-disabled');
@@ -158,9 +157,9 @@ $(function () {
   }
 
   if (settings.has('client_id') && settings.has('secret_key')) {
-    var $client_id = $('[name="client_id"]');
+    let $client_id = $('[name="client_id"]');
     $client_id.val(settings.get('client_id'));
-    var $secret_key = $('[name="secret_key"]');
+    let $secret_key = $('[name="secret_key"]');
     $secret_key.val(settings.get('secret_key'));
 
     $tabs.first().click();
@@ -176,8 +175,8 @@ $(function () {
 
     $set_form.addClass('is-disabled');
 
-    var client_id = $client_id.val();
-    var secret_key = $secret_key.val();
+    let client_id = $client_id.val();
+    let secret_key = $secret_key.val();
 
     if (!navigator.onLine) {
       $set_form.removeClass('is-disabled');
@@ -206,6 +205,10 @@ $(function () {
   // Stamp
   // =========================================================
 
+  const get_filename = function (full_path) {
+    return full_path.split('\\').pop().split('/').pop();
+  };
+
   $stamp_form.submit(function (e) {
     e.preventDefault();
 
@@ -224,8 +227,8 @@ $(function () {
 
         $stamp_form.addClass('is-disabled');
 
-        var client_id = $client_id.val();
-        var secret_key = $secret_key.val();
+        let client_id = $client_id.val();
+        let secret_key = $secret_key.val();
 
         if (!navigator.onLine) {
           $stamp_form.removeClass('is-disabled');
@@ -242,12 +245,10 @@ $(function () {
 
           display_notification('Please wait and do not close this popup as your stamping is being processed', 'success');
 
-          var blockchain = $blockchain.val();
-
-          currentStamp.blockchain = blockchain;
+          currentStamp.blockchain = $blockchain.val();
           currentStamp.data = data;
 
-          calc_hash(currentStamp.data, retrieved_hash);
+          calc_hash(currentStamp.data, retrieved_hash, get_filename(fileNames[0]));
 
         });
 
